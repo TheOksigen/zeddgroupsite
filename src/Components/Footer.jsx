258ecb21f899e8dbc9a590015437999e0c404caf
TheOksigen <david@div.edu.az>
2024-01-12 19:08:45 +0400

    modified:   src/Components/Footer.jsx 	modified:   src/output.css

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-
-
-      <footer className="bg-white dark:bg-gray-900 bottom-0">
+      <footer className=" bg-black bottom-0">
         <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
